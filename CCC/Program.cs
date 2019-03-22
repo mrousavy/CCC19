58d9a8b763d49ad8dd8a354a5f47a31088777edf
Marc Rousavy <marcrousavy@hotmail.com>
2019-03-22 08:12:13 +0100

    create project

---
+++ CCC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace CCC
+{
+    public class Program
+    {
+        public const string Filename = "input.txt";
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var lines = File.ReadAllLines(Filename);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string line = lines[i];
+                    Console.WriteLine($"Line {i}: {line}");
+                }
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
